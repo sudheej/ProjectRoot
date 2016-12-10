06eeb958558b3645cd27a2071fdab4523cce6716
Sudheej Sudhakaran <Sudheej Sudhakaran>
2016-12-10 17:25:03 +0530

    Unit Test Cases Version 1

--- Root/RootTestCases/UnitTest1.cs
+++ Root/RootTestCases/UnitTest1.cs
@@ -12,13 +12,42 @@ namespace RootTestCases
     [TestClass]
     public class TestCase1
     {
-        [TestMethod]
-        public void HomeTabTest()
-        {
-            HomeController controller = new HomeController();
-            ViewResult result = controller.Index() as ViewResult;
-            Assert.AreEqual("About", result.ViewData);
-           
+     
+
+            [TestMethod]
+            public void Index()
+            {
+                // Arrange
+                HomeController controller = new HomeController();
+                // Act
+                ViewResult result = controller.Index() as ViewResult;
+                // Assert
+                Assert.IsNotNull(result);
+            }
+
+            [TestMethod]
+            public void About()
+            {
+                // Arrange
+                HomeController controller = new HomeController();
+                // Act
+                ViewResult result = controller.About() as ViewResult;
+                // Assert
+                Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            }
+
+            [TestMethod]
+            public void Contact()
+            {
+                // Arrange
+                HomeController controller = new HomeController();
+                // Act
+                ViewResult result = controller.Contact() as ViewResult;
+                // Assert
+                Assert.IsNotNull(result);
+            }
+
+
+
         }
-    }
 }
